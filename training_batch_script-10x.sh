f12ad0b0b8c3e36bcc270fd13dab1fdf3e366e3c
Andrew Weisman <andrew.weisman@nih.gov>
2022-04-27 14:46:38 -0400

    Finish 10x feature extraction on 69 images; about to start 10x training, added batch script for this

---
+++ training_batch_script-10x.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+#SBATCH --cpus-per-task=8
+#SBATCH --partition=gpu
+#SBATCH --gres=gpu:p100:1
+#SBATCH --mem=20G
+#SBATCH --time=04:00:00
+#SBATCH -o ./logs/training-2022-04-27-10x.log
+#SBATCH -e ./logs/training-2022-04-27-10x.log
+
+echo "Started: $(date)"
+
+# Setup
+source /data/weismanal/miniconda3/etc/profile.d/conda.sh
+. /home/weismanal/projects/idibell/links/clam_installation/clam_env.sh
+working_dir="/home/weismanal/projects/idibell/repo"
+
+# Enter the appropriate directory
+cd $working_dir
+
+# Run the main script
+
+mydate="2022-04-27"
+target_resolution="10x"
+preset="bwh_resection"
+
+python $CLAM/main.py --drop_out --early_stopping --lr 2e-4 --k 10 --label_frac 1 --exp_code ${mydate}-${target_resolution} --weighted_sample --bag_loss ce --inst_loss svm --task idibell --model_type clam_sb --log_data --subtyping --data_root_dir $working_dir/results/${target_resolution}/${preset} --results_dir $working_dir/results/${target_resolution}/${preset}/training
+
+echo "Completed: $(date)"
